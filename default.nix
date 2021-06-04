e34cf181c259bb920690b9a0a5d1578c7ebb7567
jecaro <jeancharles.quillet@gmail.com>
2021-06-04 17:50:58 +0200

    make it works with current version of nixpkgs

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc802" }: let
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc8104" }: let
   ghc = nixpkgs.pkgs.haskell.packages.${compiler};
   npm = import ./npm {};
 in
-  ghc.callPackage ./package.nix {
+  ghc.callCabal2nix "ldap-client" ./. {
     mkDerivation = args: ghc.mkDerivation(args // {
       buildTools = (if args ? buildTools then args.buildTools else []) ++ [ npm.nodePackages.ldapjs ];
     });
--- shell.nix
+++ shell.nix
@@ -1,21 +1,21 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc802" }: let
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc8104" }: let
   inherit (nixpkgs) pkgs;
   haskell = pkgs.haskell.packages.${compiler};
 
   ghc = haskell.ghcWithPackages(ps: [
-    ps.hdevtools ps.doctest ps.hspec-discover ps.hlint ps.ghc-mod
+    ps.doctest ps.hspec-discover ps.hlint ps.haskell-language-server
   ]);
   npm = import ./npm {};
 
   this = import ./default.nix { inherit nixpkgs compiler; };
 in
-  pkgs.stdenv.mkDerivation rec {
-    name = this.pname;
+  pkgs.mkShell rec {
+    inputsFrom = [ this.env ];
     buildInputs = [
       ghc
       haskell.cabal-install
       npm.nodePackages.ldapjs
-    ] ++ this.env.buildInputs;
+    ];
     shellHook = ''
       ${this.env.shellHook}
       cabal configure --enable-tests --package-db=$NIX_GHC_LIBDIR/package.conf.d
